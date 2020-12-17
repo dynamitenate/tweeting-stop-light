b09b50ea802f5d25d17fe70aeab72d708c68a8cb
Nathan Raine <nraine18@gmail.com>
2020-12-16 19:12:00 -0600

    moved some logic to separate files

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,16 @@ extern crate serde_json;
 extern crate chrono;
 extern crate serde;
 
-use oauth1::Token;
+mod request;
+mod keys;
+
 use reqwest::Client;
-use reqwest::header::Authorization;
 use serde_json::Value as JsonValue;
-use std::collections::HashMap;
-use std::borrow::Cow;
 use std::str::FromStr;
 use std::time::{Duration, Instant};
 use std::thread::sleep;
 use chrono::Local;
-use std::io::BufReader;
-use std::fs::File;
-use std::path::Path;
 use serde::Deserialize;
-use std::error::Error;
 
 #[derive(Debug, Clone, Copy)]
 enum LightColor {
@@ -61,14 +56,14 @@ fn main() {
 fn request_loop() {
     // repetitive stuff
     let client = Client::new();
-    let keys = get_keys_from_file("keys.json").unwrap();
+    let keys = keys::get_keys_from_file("keys.json").unwrap();
 
     // actual loop
     let mut request_time;
     let mut request_number = 1;
     loop {
         request_time = Instant::now();
-        let response = send_request(&client, &keys);
+        let response = request::send_request(&client, &keys);
         let json = response_to_json(&response[..]);
         if !json.is_none() {
             let new_twitter_mention = get_mention_from_json(json.unwrap());
@@ -83,13 +78,6 @@ fn request_loop() {
         sleep(Duration::new(15, 0));
         request_number += 1;
     }
-}
-
-fn get_keys_from_file<P: AsRef<Path>>(path: P) -> Result<Keys, Box<dyn Error>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let keys = serde_json::from_reader(reader)?;
-    return Ok(keys);
 }
 
 fn get_light_color(hashtags: &Vec<&str>) -> Option<LightColor> {
@@ -135,28 +123,4 @@ fn response_to_json(response: &str) -> Option<JsonValue> {
         return Some(json.unwrap());
     }
     return None;
-}
-
-fn send_request(client: &Client, keys: &Keys) -> String {
-    let url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json";
-    let mut params_map: HashMap<&str, Cow<'_, str>> = HashMap::new();
-    params_map.insert("count", Cow::from("1"));
-    params_map.insert("trim_user", Cow::from("1"));
-    let response_text = client.get(url)
-        .header(Authorization(oauth1::authorize(
-            "GET",
-            url,
-            &Token::new(&keys.api_key, &keys.api_secret_key),
-            Some(&Token::new(&keys.user_key, &keys.user_secret_key)),
-            Some(params_map),
-        )))
-        .query(&[
-            ("count", "1"),
-            ("trim_user", "1")
-        ])
-        .send()
-        .expect("Could not make the request!")
-        .text()
-        .expect("Could not read response text!");
-    return response_text;
 }
---
+++ src/request.rs
@@ -0,0 +1,34 @@
+extern crate oauth1;
+extern crate reqwest;
+
+use super::keys::Keys;
+
+use oauth1::Token;
+use reqwest::Client;
+use reqwest::header::Authorization;
+use std::collections::HashMap;
+use std::borrow::Cow;
+
+pub fn send_request(client: &Client, keys: &Keys) -> String {
+    let url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json";
+    let mut params_map: HashMap<&str, Cow<'_, str>> = HashMap::new();
+    params_map.insert("count", Cow::from("1"));
+    params_map.insert("trim_user", Cow::from("1"));
+    let response_text = client.get(url)
+        .header(Authorization(oauth1::authorize(
+            "GET",
+            url,
+            &Token::new(&keys.api_key, &keys.api_secret_key),
+            Some(&Token::new(&keys.user_key, &keys.user_secret_key)),
+            Some(params_map),
+        )))
+        .query(&[
+            ("count", "1"),
+            ("trim_user", "1")
+        ])
+        .send()
+        .expect("Could not make the request!")
+        .text()
+        .expect("Could not read response text!");
+    return response_text;
+}
