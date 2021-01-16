3b19dfd9698ddf2685a1742f11f6bd2419a8e721
Nathan Raine <nraine18@gmail.com>
2021-01-16 17:53:33 -0600

    request header made more efficiently

--- src/request.rs
+++ src/request.rs
@@ -1,9 +1,26 @@
 use super::keys::Keys;
 use oauth1::Token;
 use reqwest::blocking::Client;
-use reqwest::header::{AUTHORIZATION};
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
 use std::collections::HashMap;
 use std::borrow::Cow;
+
+fn get_oauth_header(method: &str, url: &str, keys: &Keys, params: &[(&str, &str)]) -> HeaderMap {
+    let mut params_map: HashMap<&str, Cow<'_, str>> = HashMap::new();
+    for (key, value) in params {
+        params_map.insert(key, Cow::from(&value[..]));
+    }
+    let oauth = oauth1::authorize(
+        method,
+        url,
+        &Token::new(&keys.api_key, &keys.api_secret_key),
+        Some(&Token::new(&keys.user_key, &keys.user_secret_key)),
+        Some(params_map),
+    );
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&oauth).unwrap());
+    return headers;
+}
 
 pub fn get_tls_client() -> Option<Client> {
     let client_builder = reqwest::blocking::Client::builder()
@@ -17,21 +34,14 @@ pub fn get_tls_client() -> Option<Client> {
 
 pub fn send_request(client: &Client, keys: &Keys) -> Option<String> {
     let url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json";
-    let mut params_map: HashMap<&str, Cow<'_, str>> = HashMap::new();
-    params_map.insert("count", Cow::from("1"));
-    params_map.insert("trim_user", Cow::from("1"));
+    let params = &[
+        ("count", "1"),
+        ("trim_user", "1")
+    ];
+    let header = get_oauth_header("GET", url, &keys, params);
     let request = client.get(url)
-        .header(AUTHORIZATION, oauth1::authorize(
-            "GET",
-            url,
-            &Token::new(&keys.api_key, &keys.api_secret_key),
-            Some(&Token::new(&keys.user_key, &keys.user_secret_key)),
-            Some(params_map),
-        ))
-        .query(&[
-            ("count", "1"),
-            ("trim_user", "1")
-        ]);
+        .headers(header)
+        .query(params);
     let response_text = match request.send() {
         Ok(response) => {
             match response.text() {
